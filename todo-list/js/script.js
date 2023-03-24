fd631c7fb5072c0a2c6ac44f09e0e43943aedd11
Gustavo Oliveira <123515463+gustaoliveira1@users.noreply.github.com>
2023-03-24 09:14:49 -0300

    Ultimos acertos no projeto

--- todo-list/js/script.js
+++ todo-list/js/script.js
@@ -9,12 +9,14 @@ const cancel_edit = document.querySelector("#cancel-edit")
 const edit_form = document.querySelector("#edit-form")
 const edit_input = document.querySelector("#edit-input")
 
-var tarefas = []
-var id_nova_tarefa = 0
-var id_tarefa_selecionada = 0
-var content_tarefa_edited = ""
+var tarefa_editada = null
 
 // Funções
+function toggleHide() {
+    container_principal.classList.toggle("hide")
+    edit_form_container.classList.toggle("hide")
+}
+
 function adicionarItemTela(elemento) {
     const firstChild = lista_tarefas.firstChild
     lista_tarefas.insertBefore(elemento, firstChild)
@@ -23,11 +25,9 @@ function adicionarItemTela(elemento) {
 function criarNovoItem(text) {
     let container_tarefa = document.createElement("div")
     container_tarefa.classList.add("tarefa")
-    container_tarefa.id = id_nova_tarefa
 
     let tarefa = document.createElement("h3")
     tarefa.innerText = text
-    tarefa.classList.add("description")
     container_tarefa.appendChild(tarefa)
 
     let container_buttons = document.createElement("div")
@@ -61,19 +61,12 @@ form.addEventListener("submit", (event) => {
     let descricao_tarefa = input_tarefa.value
 
     if (descricao_tarefa) {
-        id_nova_tarefa ++
-        let nova_tarefa = {
-            id: id_nova_tarefa,
-            descricao: descricao_tarefa,
-        }
-        tarefas.push(nova_tarefa)
 
-        estrutura_nova_tarefa = criarNovoItem(nova_tarefa.descricao)
+        estrutura_nova_tarefa = criarNovoItem(descricao_tarefa)
         adicionarItemTela(estrutura_nova_tarefa)
 
         input_tarefa.value = ""
         input_tarefa.focus()
-        
     }
 })
 
@@ -88,15 +81,25 @@ document.addEventListener("click", (event) => {
         tarefa.remove()
     }
     else if (elemento.classList.contains("edit-button")) {
-        container_principal.classList.add("hide")
-        edit_form_container.classList.remove("hide")
-        let descricao_tarefa = tarefa.querySelector("h3")
+        toggleHide()
+        tarefa_editada = tarefa.querySelector("h3")
     }
+})
+
+edit_form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    nova_descricao = edit_input.value
+
+    if (nova_descricao) {
+        tarefa_editada.innerText = nova_descricao
+        toggleHide()
+    }
+    edit_input.value = ""
+    edit_input.focus()
 })
 
 cancel_edit.addEventListener("click", (event) => {
     event.preventDefault()
-
-    container_principal.classList.remove("hide")
-    edit_form_container.classList.add("hide")
+    toggleHide()
 })
